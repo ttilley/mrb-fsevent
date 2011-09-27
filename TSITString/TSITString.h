1e0caa43b999618fab8764314e011696af51b578
Travis Tilley <ttilley@gmail.com>
2011-09-27 01:25:26 -0400

    Initial Commit

---
+++ TSITString/TSITString.h
@@ -0,0 +1,13 @@
+//
+//  TSITString.h
+//  TSITString
+//
+//  Created by Travis Tilley on 9/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSITString : NSObject
+
+@end
---
+++ TSITString/TSITString.m
@@ -0,0 +1,23 @@
+//
+//  TSITString.m
+//  TSITString
+//
+//  Created by Travis Tilley on 9/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TSITString.h"
+
+@implementation TSITString
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
