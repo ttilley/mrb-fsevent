d0e067734c7876250b52943faf8372437f0dcdc4
Travis Tilley <ttilley@gmail.com>
2011-09-27 17:46:04 -0400

    initial tagged string code supporting tnetstrings and otnetstrings

---
+++ TSITStringTestApp/main.m
@@ -0,0 +1,32 @@
+//
+//  main.m
+//  TSITStringTestApp
+//
+//  Created by Travis Tilley on 9/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <CoreFoundation/CoreFoundation.h>
+#import <Foundation/Foundation.h>
+#import "../TSITString/TSICTString.h"
+
+
+int main (int argc, const char * argv[])
+{
+    @autoreleasepool {
+        NSString* thisFile = [NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding];
+        NSString* thisDir = [thisFile stringByDeletingLastPathComponent];
+        NSString* plistPath = [thisDir stringByAppendingPathComponent:@"test.plist"];
+        
+        id object = [NSArray arrayWithContentsOfFile:plistPath];
+        
+        for (int i=0; i < 1000; i++) {
+            @autoreleasepool {
+                CFDataRef data = TSICTStringCreateRenderedDataFromObjectWithFormat(object, kTSITStringFormatTNetstring);
+                CFRelease(data);
+            }
+        }
+    }
+    
+    return 0;
+}
